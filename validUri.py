2ae2e99ee451c1345fcbbc86dd2aee24cd0f9d5d
temevh <temehama@gmail.com>
2023-01-26 15:22:22 +0200

    pretty much final, added description.txt

--- validUri.py
+++ validUri.py
@@ -12,31 +12,39 @@ class checkIdentity:
     def uriChecker(self):
         isValid = re.match("^visma-identity://.*", self.uri)
         if isValid:
-            print("URL IS VALID")
+            #print("URL IS VALID")
             parsed_uri = urlparse(self.uri)
             self.path = parsed_uri.netloc
-            print(self.path)
-
+            # print(self.path)
         else:
-            raise Exception("Check URI scheme")
+            raise Exception("CHECK URI SCHEME")
 
         if self.path not in ['login', 'confirm', 'sign']:
-            raise Exception("Check URI path")
+            raise Exception("CHECK URI PATH")
 
         parameters = re.search(r'\?(.*)', self.uri).group(1)
+        # print(parameters)
+
         for p in parameters.split("&"):
             key, value = p.split("=")
+            if value == "":
+                raise Exception("CHECK PARAMETERS")
+            if key == "paymentnumber":  # make sure paymentnumber is stored as an integer
+                value = int(value)
             self.params[key] = value
 
+        # print(self.params)
         if self.path == "login":
-            if "source" not in self.params:
+            # Check that the "source parameter" exists AND that it is a string
+            if "source" not in self.params or self.params["source"].isalpha() == False:
                 raise Exception("Check source parameter")
+
         elif self.path == "confirm":
+            # Check that the source and paymentnumber parameters exist
             if "source" not in self.params or "paymentnumber" not in self.params:
                 raise Exception(
                     "Check source and/or paymentnumber parameter(s)")
-            if not self.params["paymentnumber"].isdigit():
-                raise Exception("Check paymentnumber (must be digit)")
+
         elif self.path == "sign":
             if "source" not in self.params or "documentid" not in self.params:
                 raise Exception("Check source and/or document id parameter(s)")
@@ -53,7 +61,12 @@ class testClient:
         return self.id.params
 
 
-client1 = testClient(
-    "visma-identity://login?source=severa")
-print(client1.get_path())
-print(client1.get_params())
+testUri1 = "visma-identity://sign?source=vismasign&documentid=105ab44"
+
+client1 = testClient(testUri1)
+print("path is", client1.get_path())
+print("parameters are", client1.get_params())
+
+# TODO
+# Parameter (existence) checking
+# Change paymentnumber str -> int DONE
